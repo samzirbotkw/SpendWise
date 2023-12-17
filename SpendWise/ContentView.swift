6d230ebfe7bc35c0c472946eace014cb1c3572f4
samzirbotkw <83039189+samzirbo@users.noreply.github.com>
2023-12-17 19:10:08 +0200

    Initial Commit

---
+++ SpendWise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpendWise
+//
+//  Created by George Zirbo on 17.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpendWise/SpendWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendWiseApp.swift
+//  SpendWise
+//
+//  Created by George Zirbo on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpendWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
